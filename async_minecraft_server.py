c85df205e4f4c9669c34721be9edfe19d4b32f7f
CalvinSprouse <sprousecal@gmail.com>
2021-06-13 15:39:29 -0700

    needs property editing

---
+++ async_minecraft_server.py
@@ -0,0 +1,246 @@
+from bs4 import BeautifulSoup
+from multiprocessing import Process
+import aiohttp
+import aiofiles
+import asyncio
+import coloredlogs
+import logging
+import math
+import os
+import psutil
+import re
+import shutil
+import subprocess
+import sys
+import wget
+
+# configure logger
+coloredlogs.install(level="DEBUG")
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.DEBUG,
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
+logger = logging.getLogger("py-server")
+logging.getLogger("asyncio").disabled = True
+
+
+def get_jar_name(version: str) -> str:
+    """Normalizes the jar naming scheme, returns minecraft_server.{version}.jar"""
+    return f"minecraft_server.{version}.jar"
+
+
+# custom exceptions
+class ExceedMaxServerCountException(Exception):
+    pass
+
+
+class ServerNameTakenException(Exception):
+    pass
+
+
+class ServerDoesNotExistException(Exception):
+    pass
+
+
+class VersionException(Exception):
+    pass
+
+
+# classes
+class ContextManager:
+    def __init__(self, new_path):
+        self.new_path = os.path.expanduser(new_path)
+
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+        logger.debug(f"Entering {self.new_path} from {self.saved_path}")
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
+        logger.debug(f"Exiting {self.new_path} to {self.saved_path}")
+
+
+class ServerMaker:
+    def __init__(self, save_location: str, max_servers: int = 5):
+        """
+        A server creation object that has a save location and can make servers.
+        :param save_location: The base location for servers to be saved at.
+        """
+        self.server_location = os.path.abspath(save_location)
+        os.makedirs(self.server_location, exist_ok=True)
+        self.max_servers = max_servers
+        if self.max_servers < 0:
+            self.max_servers = abs(self.max_servers)
+        elif self.max_servers == 0:
+            self.max_servers = None
+        logger.debug(self.__repr__())
+
+    def get_number_of_servers(self) -> int:
+        """ Get the number of servers already saved to this location. """
+        return len(os.listdir(self.server_location))
+
+    async def make_server(self, server_name: str, server_version: str = None, overwrite=False):
+        """
+        Creates a server from where the ServerMaker is operating (save_location).
+        :param server_name: Name for the server/file.
+        :param server_version: Specified version in format #.#.# if left blank will default to most up to date version.
+        :param overwrite: If true will replace a server of the same name, otherwise will throw an error for duplicate names.
+        """
+        if not self.max_servers or self.max_servers > self.get_number_of_servers() or (
+                server_name in os.listdir(self.server_location) and overwrite):
+            server_save_location = os.path.join(self.server_location, server_name)
+            logger.debug(f"Creating server {server_save_location}, v={server_version}, o={overwrite}")
+            if os.path.exists(server_save_location):
+                if overwrite:
+                    logger.info(f"Removing {server_save_location} for overwrite")
+                    shutil.rmtree(server_save_location)
+                else:
+                    raise ServerNameTakenException()
+            os.makedirs(server_save_location, exist_ok=True)
+
+            if not server_version:
+                server_version = await self.get_current_minecraft_version()
+
+            await self.download_jar(
+                os.path.join(self.server_location, server_name, get_jar_name(server_version)),
+                server_version)
+            with ContextManager(server_save_location):
+                subprocess.run(f"java -Xms1G -Xmx1G -jar {get_jar_name(server_version)}",
+                               stdout=subprocess.DEVNULL)
+                eula_lines = open("eula.txt", "r").readlines()[:2] + ["eula=true\n"]
+                open("eula.txt", "w").writelines(eula_lines)
+            logger.debug(f"Eula accepted for server {server_name}")
+        else:
+            raise ExceedMaxServerCountException()
+
+    @staticmethod
+    async def fetch_html(url: str, session: aiohttp.ClientSession, **kwargs) -> str:
+        """GET Request wrapper to fetch page html"""
+        response = await session.request(method="GET", url=url, **kwargs)
+        response.raise_for_status()
+        logger.info(f"Got response {response.status} for {url}")
+        # await session.close()
+        text = await response.text()
+        return text
+
+    @staticmethod
+    async def get_current_minecraft_version() -> str:
+        """Retrieves the current minecraft version from minecraft.net"""
+        response = await ServerMaker.fetch_html(r"https://www.minecraft.net/en-us/download/server",
+                                                aiohttp.ClientSession())
+        return re.search(r"minecraft_server.(?P<version>(.\d*)*).jar",
+                         BeautifulSoup(response, "html.parser").find("div", {"class": "minecraft-version"}).find(
+                             "a").text).group("version")
+
+    @staticmethod
+    async def download_jar(jar_save_location: str, jar_version: str):
+        """Download a jar file from mcversions.net to a location"""
+        response = await ServerMaker.fetch_html(url=f"https://mcversions.net/download/{jar_version}",
+                                                session=aiohttp.ClientSession())
+        try:
+            download_link = [link for link in
+                             BeautifulSoup(response,
+                                           "html.parser").find("div", {"class": "downloads"}).find_all("a", href=True)
+                             if "server" in link["download"]][0]["href"]
+        except IndexError:
+            raise VersionException()
+        logger.debug(f"Downloading server jar from {download_link} to {jar_save_location}")
+        wget.download(download_link, jar_save_location)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def __repr__(self):
+        return f"ServerMaker loc={self.server_location}, max={self.max_servers}"
+
+
+class ServerLoader:
+    def __init__(self, save_location: str, server_name: str):
+        """
+        A server loader loads a specific server from a save location and edits/runs it.
+        :param save_location: The directory servers are saved to.
+        """
+        self.save_location = os.path.abspath(save_location)
+        self.server_location = os.path.join(self.save_location, server_name)
+        assert os.path.exists(self.save_location)
+
+        self.server = None
+        self.properties = None
+        self.mem_allocation = None
+        self.server_process = None
+
+        self.set_server(server_name)
+
+    def set_server(self, server_name: str):
+        self.server = server_name
+        self.server_location = os.path.join(self.save_location, server_name)
+        if not os.path.exists(self.server_location):
+            raise ServerDoesNotExistException()
+        await self.load_properties()
+
+    async def load_properties(self):
+        properties_contents = await self.get_properties_file_lines()
+        self.properties = {line.strip().split("=")[0]: line.strip().split("=")[1] for line in properties_contents
+                           if not line.startswith("#")}
+
+    async def save_server(self):
+        properties_contents = await self.get_properties_file_lines()
+        async with aiofiles.open(os.path.join(self.server_location, "server.properties"), "w") as properties_file:
+            await properties_file.writelines(
+                [f"{key.split('=')[0]}={self.properties[key.split('=')[0]]}\n" if "=" in key else key for key in
+                 properties_contents])
+
+    async def get_properties_file_lines(self) -> []:
+        async with aiofiles.open(os.path.join(self.server_location, "server.properties"), "r") as properties_file:
+            properties_contents = await properties_file.readlines()
+        return properties_contents
+
+    def start_server(self, mem_allocation: int):
+        self.set_mem_allocation(mem_allocation)
+        self.server_process = Process(target=self.run_server)
+        self.server_process.start()
+        return self.server_process
+
+    def run_server(self):
+        with ContextManager(self.server_location):
+            os.system(f"java -Xms{self.mem_allocation}G -Xmx{self.mem_allocation} -jar {get_jar_name(self.get_current_version())}")
+
+    def stop_server(self):
+        if self.is_running():
+            self.server_process.close()
+
+    def is_running(self):
+        if self.server_process:
+            return self.server_process.is_alive()
+        return False
+
+    def get_process(self):
+        return self.server_process
+
+    def set_mem_allocation(self, mem_allocation: int):
+        if not self.is_running():
+            self.mem_allocation = min(mem_allocation, self.get_max_mem_allocation())
+
+    def get_current_version(self):
+        return re.search(r"minecraft_server.(?P<version>(.\d*)*).jar",
+                         [jar for jar in os.listdir(self.server_location) if ".jar" in jar][0],
+                         re.IGNORECASE).group("version")
+
+    @staticmethod
+    def get_max_mem_allocation():
+        return int((psutil.virtual_memory().available / math.pow(10, 9)) * 0.5)
+
+
+async def main():
+    with ServerMaker(save_location="training") as maker:
+        await maker.make_server(server_name="Test", overwrite=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
